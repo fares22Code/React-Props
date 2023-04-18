e53b25215b8eb445e6135ce0e3266688a2bae906
fares22Code <124197202+fares22Code@users.noreply.github.com>
2023-04-18 03:15:12 +0100

    React-Props
    React-Props

---
+++ src/Component/App/App.js
@@ -0,0 +1,13 @@
+import Profile from '../Profile/Profile.js';
+import pImg from '../../ProfilePic.jpg';
+import "./app.css";
+function App () {
+
+  return (
+    <div className="App">
+      <Profile fullName="Fares TROJET" bio="About Me: Experienced HTML,CSS,JavaScript Developer with two years in the industry. Proficient with React. Used problem-solving aptitude to enhance applications performance. Created data visualization tools and integrated designs. Increased speed of product lifecycle and 100% of the time delivered projects within deadline. " profession="- Web Developer -" imge={pImg} />
+    </div>
+    );
+}
+
+export default App;
